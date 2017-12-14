d135a68d30f8377374e438e1f86d8e6f824de887
pierre-lamarche <lamarche.p@gmail.com>
2017-12-14 12:09:50 +0100

    New commit of codes for SE 3 article

---
+++ StatisticsExplained3/generate figures and graphs.R
@@ -0,0 +1,89 @@
+############################################################################################################
+# Scripts for computing figures and generating graphs in Statistics Explained article
+# http://ec.europa.eu/eurostat/statistics-explained/index.php?title=Interaction_of_household_income,_consumption_and_wealth_-_statistics_on_direct_and_indirect_taxation
+# Run on R 64-bits 3.4.1
+
+library(tidyverse)
+library(Cairo)
+library(eurostat)
+library(reshape2)
+
+col1 <- rgb(250, 165, 25, maxColorValue = 255)
+col1_faded <- rgb(251, 200, 117, maxColorValue = 255)
+col2 <- rgb(40, 110, 180, maxColorValue = 255)
+col2_faded <- rgb(113, 168, 223, maxColorValue = 255)
+col3 <- rgb(240, 99, 34, maxColorValue = 255)
+col3_faded <- rgb(246, 162, 123, maxColorValue = 255)
+col4 <- rgb(185, 195, 30, maxColorValue = 255)
+col5 <- rgb(93, 179, 64, maxColorValue = 255)
+
+#################################################################################################################################################
+### FIGURE 1
+#################################################################################################################################################
+
+# In National Accounts
+
+vatNA <- get_eurostat("gov_10a_taxag", time = "num")
+vatNA <- filter(vatNA,
+                sector == "S13" & na_item == "D211" & unit == "MIO_EUR")
+vatNA <- rename(vatNA,
+                values_vat = values)
+cons <- get_eurostat("nasa_10_nf_tr", time = "num")
+cons <- filter(cons,
+               sector %in% c("S14","S14_S15") & na_item == "P31" & unit == "CP_MEUR" & direct == "PAID")
+cons <- dcast(geo+time~sector, data = cons, value.var = "values")
+
+correction_sector <- cons %>%
+  filter(!is.na(S14)) %>%
+  group_by(time) %>%
+  summarise(correction_sector = mean(S14/S14_S15))
+cons <- merge(cons, correction_sector, by = "time")
+
+cons <- mutate(cons,
+               values_cons = ifelse(is.na(S14), S14_S15*correction_sector, S14))
+vatNA <- merge(select(vatNA, geo, time, values_vat), select(cons, geo, time, values_cons))
+vatNA <- vatNA %>%
+  mutate(vat_rate = round(values_vat/(values_cons - values_vat)*100, digits = 2)) %>%
+  filter(time == 2010) %>%
+  mutate(geo = as.character(geo)) %>%
+  filter(nchar(geo) == 2) %>%
+  arrange(geo)
+
+# In HBS
+
+list_cty <- c("AT","BE","BG","CY","CZ","DE","DK","EE","EL","ES","FI","FR","HR","HU","IE","IT","LT","LU","LV",
+              "MT","PL","PT","RO","SE","SI","SK","UK")
+
+avg_consumption <- get_eurostat("hbs_exp_t111", time_format = "num")
+avg_consumption <- filter(avg_consumption, stat == "DMOM" & time == 2010 & geo %in% list_cty & currency == "EUR")
+avg_consumption <- mutate(avg_consumption,
+                          consumption = values)
+
+str_consumption <- get_eurostat("hbs_str_t211", time_format = "num")
+str_consumption <- mutate(str_consumption,
+                          coicop = as.character(coicop),
+                          coicop = substr(coicop, 3, nchar(coicop)),
+                          lv = nchar(coicop) - 1, 
+                          share = values/1000)
+str_consumption <- filter(str_consumption, geo %in% list_cty & time == 2010 & lv == 2) ## we could take lv == 3 but totals do not add up to 1 - we don't have the full consumption structure at this level of details
+
+consumption <- merge(str_consumption, avg_consumption, by = c("geo", "time"))
+consumption <- mutate(consumption,
+                      consumption = consumption*share)
+
+rateVat <- get_eurostat("icw_tax_10", time_format = "num")
+rateLv2 <- rateVat %>%
+  mutate(coicop = substr(as.character(coicop), 3, 5)) %>%
+  filter(nchar(coicop) == 3) %>%
+  rename(vatRate = values)
+consumption <- merge(consumption, rateLv2, by = c("geo","time","coicop"))
+consumption <- mutate(consumption,
+                      vat = consumption*vatRate/(100 + vatRate))
+vat <- consumption %>%
+  group_by(geo) %>%
+  summarise(vat = sum(vat),
+            consumption_pc = sum(consumption))
+
+vat <- mutate(vat,
+              vatRate = round(vat/(consumption_pc - vat)*100, digits = 2))
+
