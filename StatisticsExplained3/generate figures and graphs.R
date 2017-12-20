563f2e7b17da48ea9aeaaf0cede36bebb67148c3
pierre-lamarche <lamarche.p@gmail.com>
2017-12-20 11:53:45 +0100

    Figures 11 and 12

--- StatisticsExplained3/generate figures and graphs.R
+++ StatisticsExplained3/generate figures and graphs.R
@@ -363,5 +363,39 @@ barplot(t(figure9[,2:6]), beside = TRUE, col = c(col1, col2, col3, col4, col5), 
         names.arg = figure9$geo, cex.names = 0.5,
         args.legend = list(x = "topleft", bty = "n", border = NA, cex = 0.5))
 
+#################################################################################################################################################
+### FIGURE 11
+#################################################################################################################################################
+
+tax_rate_hhtyp <- get_eurostat("icw_tax_08", time_format = "num")
+tax_rate_hhtyp <- filter(tax_rate_hhtyp, quantile == "MED" & hhcomp != "TOTAL")
+
+figure11 <- dcast(tax_rate_hhtyp, geo~hhcomp, value.var = "values")
+figure11 <- merge(figure11, countryOrder, by.x = "geo", by.y = "Country")
+figure11 <- arrange(figure11, Protocol_order)
+
+barplot(t(figure11[,2:7]), beside = TRUE, col = c(col1, col1_faded, col2, col2_faded,
+                                                  col3, col3_faded), main = NA,
+        border = NA, legend.text = c("One adult","One adult with dependent children","Two adults",
+                                     "Two adults with dependent children","Three adults and more","Three adults and more with dependent children"),
+        names.arg = figure11$geo, cex.names = 0.5,
+        args.legend = list(x = "topleft", bty = "n", border = NA, cex = 0.5))
+
+#################################################################################################################################################
+### FIGURE 12
+#################################################################################################################################################
+
+tax_rate_ageRP <- get_eurostat("icw_tax_07", time_format = "num")
+tax_rate_ageRP <- filter(tax_rate_ageRP, quantile == "MED" & age != "TOTAL")
+
+figure12 <- dcast(tax_rate_ageRP, geo~age, value.var = "values")
+figure12 <- merge(figure12, countryOrder, by.x = "geo", by.y = "Country")
+figure12 <- arrange(figure12, Protocol_order)
+
+barplot(t(figure12[,2:5]), beside = TRUE, col = c(col1, col2, col3, col4), main = NA,
+        border = NA, legend.text = c("Less than 30","Between 30 and 44","Between 45 and 60","60 and more"),
+        names.arg = figure12$geo, cex.names = 0.5,
+        args.legend = list(x = "topleft", bty = "n", border = NA, cex = 0.5))
+
 
 
