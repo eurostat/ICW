9c7365c3a0f415744c5618bd146d4551ea786644
pierre-lamarche <lamarche.p@gmail.com>
2018-01-12 15:03:49 +0100

    Mapping coicop and tax rates done.

--- VAT/scripts/1. Compute tax rates.R
+++ VAT/scripts/1. Compute tax rates.R
@@ -60,3 +60,97 @@ rdVAT <- mutate(rdVAT,
                                                 ifelse(!is.na(rate) & !is.na(red1) & rate == red1, "RED1",
                                                        ifelse(!is.na(rate) & !is.na(red2) & rate == red2, "RED2",
                                                               ifelse(!is.na(rate) & rate == std, "STD", "STD")))))))
+## history
+
+VAThist <- readWorksheetFromFile("vat_rates_en.xls", sheet = "Evolution of VAT rates", startRow = 4)
+country <- readWorksheetFromFile("vat_rates_en.xls", sheet = "List of VAT rates applied", startRow = 3, startCol = 4, endCol = 5, endRow = 31)
+
+# patch for Slovakia
+VAThist <- mutate(VAThist,
+                  MEMBER.STATES.AND.DATES = ifelse(MEMBER.STATES.AND.DATES == "Slovak Republic", "Slovakia", MEMBER.STATES.AND.DATES))
+
+VAThist$n_ <- 1:nrow(VAThist)
+VAThist <- merge(country, VAThist, by.x = "Member.States", by.y = "MEMBER.STATES.AND.DATES", 
+                 all.y = TRUE, sort = FALSE)
+
+VAThist <- VAThist[order(VAThist$n_),]
+VAThist <- VAThist %>%
+  fill(Code)
+VAThist <- filter(VAThist,
+                  !is.na(STANDARD.RATE))
+VAThist <- mutate(VAThist,
+                  year = substr(Member.States,1,4), 
+                  REDUCED.RATE = gsub(",",".", REDUCED.RATE),
+                  std = as.numeric(STANDARD.RATE))
+rdRt <- strsplit(VAThist$REDUCED.RATE, "\\| ")
+lmax <- max(unlist(lapply(rdRt, length)))
+rdRt <- lapply(rdRt, completeV)
+
+rdRt <- do.call("rbind", rdRt)
+VAThist <- cbind(VAThist, rdRt)
+names(VAThist)[10:15] <- paste0("rdRt", names(VAThist)[10:15])
+
+VAThist <- VAThist[,c(2,8:15)]
+VAThist <- mutate(VAThist,
+                  rdRt1 = as.character(rdRt1),
+                  rdRt2 = as.character(rdRt2),
+                  rdRt3 = as.character(rdRt3),
+                  rdRt4 = as.character(rdRt4),
+                  rdRt5 = as.character(rdRt5),
+                  rdRt6 = as.character(rdRt6),
+                  rdRt1 = gsub("-",NA,rdRt1),
+                  STD = std,
+                  SUPER = ifelse(as.numeric(rdRt1) < 5, as.numeric(rdRt1), NA),
+                  RED1 = ifelse(is.na(SUPER), as.numeric(rdRt1), as.numeric(rdRt2)),
+                  RED2 = ifelse(is.na(SUPER) < 5, as.numeric(rdRt3), as.numeric(rdRt2)))
+
+VAThist <- VAThist[,c(1:2,10:13)]
+save(VAThist, file = "VAThist.RData")
+
+year <- 2010
+
+VATSelec <- VAThist[VAThist$year <= year,]
+VATSelec <- VATSelec %>%
+  group_by(Code) %>%
+  filter(year == max(year))
+
+## import correspondence coicop-reduced rate
+
+tempFile <- tempfile()
+download.file("https://github.com/eurostat/ICW/blob/master/VAT/data/listCOICOP_withRATE.xls", destfile = tempFile)
+
+lvl1 <- readWorksheetFromFile(tempFile, sheet = "LEVEL1")
+lvl2 <- readWorksheetFromFile(tempFile, sheet = "LEVEL2")
+lvl3 <- readWorksheetFromFile(tempFile, sheet = "LEVEL3")
+lvl4 <- readWorksheetFromFile(tempFile, sheet = "LEVEL4")
+
+lvl4 <- mutate(lvl4,
+               CODE_LEVEL1 = substr(CODE_LEVEL4, 1, 2),
+               CODE_LEVEL2 = substr(CODE_LEVEL4, 1, 3),
+               CODE_LEVEL3 = substr(CODE_LEVEL4, 1, 4))
+
+lvl4 <- merge(lvl4, lvl1)
+lvl4 <- merge(lvl4, lvl2)
+lvl4 <- merge(lvl4, lvl3)
+
+lvl4 <- mutate(lvl4,
+               VAT = ifelse(!is.na(VAT_LEVEL1), VAT_LEVEL1, 
+                            ifelse(!is.na(VAT_LEVEL2), VAT_LEVEL2,
+                                   ifelse(!is.na(VAT_LEVEL3), VAT_LEVEL3,
+                                          ifelse(!is.na(VAT_LEVEL4), VAT_LEVEL4, NA)))))
+
+coicopVAT <- merge(lvl4[,c(5:6,17)], country)
+coicopVAT <- merge(coicopVAT, rdVAT[, c(1:3,10)], by.x = c("Code","VAT"), by.y = c("variable","Col1"), all.x = TRUE)
+
+coicopVAT <- coicopVAT[order(coicopVAT$Code, coicopVAT$CODE_LEVEL4),]
+
+coicopVAT <- mutate(coicopVAT,
+                    VAT_rate = ifelse(substr(CODE_LEVEL4,1,3) %in% c("041","042","063") | substr(CODE_LEVEL4,1,2) == "10", "EXEMP",
+                                      ifelse(is.na(VAT_rate), "STD", VAT_rate)))
+
+save(coicopVAT, file = "coicopVAT.RData")
+
+names(lvl1) <- paste0(names(lvl1), "_LEVEL1")
+names(lvl2) <- paste0(names(lvl2), "_LEVEL2")
+names(lvl3) <- paste0(names(lvl3), "_LEVEL3")
+names(lvl4) <- paste0(names(lvl4), "_LEVEL4")
